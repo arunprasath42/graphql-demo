3019f03db6882d196052b9c9c891c4571f2f3d60
arun.ps1 <arun.ps@accubits.com>
2023-04-14 15:51:08 +0530

    Added codebase for crud operation using graphql & mongodb

---
+++ database/database.go
@@ -0,0 +1,111 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/arunprasath42/graphql-live/graph/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect() *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	return &DB{
+		client: client,
+	}
+}
+
+func (db *DB) CreateEmployee(employee model.NewEmployee) (*model.Employee, error) {
+	collection := db.client.Database("company").Collection("employees")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res, err := collection.InsertOne(ctx, employee)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &model.Employee{
+		ID:         res.InsertedID.(primitive.ObjectID).Hex(),
+		Name:       employee.Name,
+		IsTeamLead: employee.IsTeamLead,
+	}, nil
+}
+
+//FindByID
+func (db *DB) GetEmployee(id string) (*model.Employee, error) {
+	ObjectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection := db.client.Database("company").Collection("employees")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	res := collection.FindOne(ctx, bson.M{"_id": ObjectID})
+	employee := model.Employee{}
+	res.Decode(&employee)
+	return &employee, nil
+}
+
+//updateEmployee
+func (db *DB) UpdateEmployee(id string, employee model.NewEmployee) (*model.Employee, error) {
+	collection := db.client.Database("company").Collection("employees")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	ObjectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := collection.UpdateByID(ctx, ObjectID, bson.M{"$set": employee})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &model.Employee{
+		ID:         res.UpsertedID.(primitive.ObjectID).Hex(),
+		Name:       employee.Name,
+		IsTeamLead: employee.IsTeamLead,
+	}, nil
+}
+
+//deleteEmployee
+func (db *DB) DeleteEmployee(id string) (*model.Employee, error) {
+	collection := db.client.Database("company").Collection("employees")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	ObjectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res := collection.FindOneAndDelete(ctx, bson.M{"_id": ObjectID})
+	employee := model.Employee{}
+	res.Decode(&employee)
+	return &employee, nil
+}
+
+//getEmployees
+func (db *DB) GetEmployees() ([]*model.Employee, error) {
+	collection := db.client.Database("company").Collection("employees")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var employees []*model.Employee
+	if err = cursor.All(ctx, &employees); err != nil {
+		log.Fatal(err)
+	}
+	return employees, nil
+}
---
+++ readme.md
@@ -0,0 +1,90 @@
+<!-- GOLANG-GRAPHQL-CRUD OPERATION -->
+
+# Golang GraphQL CRUD Operation
+
+This is a simple example of how to implement a CRUD operation using GraphQL and Golang.
+
+## How to run
+
+1. Install the dependencies
+
+```bash
+go get -u github.com/graphql-go/graphql
+go get -u github.com/graphql-go/handler
+```
+
+2. To run the server
+
+```bash
+go run server.go
+```
+
+3. Open the browser and go to the following URL
+
+```bash
+http://localhost:8080/
+```
+
+
+
+# GraphQL Queries
+
+1. Create Employee
+
+```graphql
+mutation AddEmployee {
+  createEmployee(input: { Name: "Arunprasath", IsTeamLead:true}){
+    _id
+    Name
+  	IsTeamLead
+  }
+}
+```
+
+2. Get Employee
+
+```graphql
+query GetEmployee {
+  employee(_id: "5e9b9b9b9b9b9b9b9b9b9b9b"){
+    _id
+    Name
+  	IsTeamLead
+  }
+}
+```
+
+3. Get All Employees
+
+```graphql
+query GetAllEmployees {
+  employees{
+    _id
+    Name
+  	IsTeamLead
+  }
+}
+```
+
+4. Update Employee
+
+```graphql
+mutation UpdateEmployee {
+  updateEmployee(_id: "5e9b9b9b9b9b9b9b9b9b9b9b", input: { Name: "Arunprasath", IsTeamLead:false}){
+    _id
+    Name
+  	IsTeamLead
+  }
+}
+```
+
+5. Delete Employee
+
+```graphql
+mutation DeleteEmployee {
+  deleteEmployee(_id: "5e9b9b9b9b9b9b9b9b9b9b9b"){
+    _id
+    Name
+  	IsTeamLead
+  }
+}
+```
